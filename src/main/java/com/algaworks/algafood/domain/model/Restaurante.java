35ae605dc25b91e9b4bfd03b077514f470c165fb
Evandro Gubert <gubertevandro33@gmail.com>
2025-08-21 21:30:09 -0300

    feature: criando constraints de validacao customizadas com implementacao de ConstraintValidator

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
 import com.algaworks.algafood.core.validation.Groups;
+import com.algaworks.algafood.core.validation.Multiplo;
 import com.algaworks.algafood.core.validation.TaxaFrete;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
@@ -36,7 +37,8 @@ public class Restaurante {
     private String nome;
 
     //@PositiveOrZero(message = "{TaxaFrete.invalida}")
-    @TaxaFrete
+    //@TaxaFrete
+    @Multiplo(numero = 5)
     @NotNull
     @Column(name = "taxa_frete")
     private BigDecimal taxaFrete;
